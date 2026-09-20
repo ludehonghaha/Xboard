9c6a062013f42d6e1f90a722d92c18f10b7e70ee
hahadh <hahadhlu@gmail.com>
2026-09-20 19:49:50 +0800

    lite: add Snell v5 server protocol model

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -70,6 +70,7 @@ class Server extends Model
     public const TYPE_NAIVE = 'naive';
     public const TYPE_HTTP = 'http';
     public const TYPE_MIERU = 'mieru';
+    public const TYPE_SNELL = 'snell';
     public const STATUS_OFFLINE = 0;
     public const STATUS_ONLINE_NO_PUSH = 1;
     public const STATUS_ONLINE = 2;
@@ -108,6 +109,7 @@ class Server extends Model
         self::TYPE_NAIVE,
         self::TYPE_HTTP,
         self::TYPE_MIERU,
+        self::TYPE_SNELL,
     ];
 
     protected $table = 'v2_server';
@@ -321,6 +323,10 @@ class Server extends Model
             'transport' => ['type' => 'string', 'default' => 'TCP'],
             'traffic_pattern' => ['type' => 'string', 'default' => ''],
             ...self::MULTIPLEX_CONFIGURATION,
+        ],
+        self::TYPE_SNELL => [
+            'version' => ['type' => 'integer', 'default' => 5],
+            'quic' => ['type' => 'boolean', 'default' => false],
         ]
     ];
 
