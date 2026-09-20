44262617ac011d550d795ba7260799e073b13096
hahadh <hahadhlu@gmail.com>
2026-09-20 16:41:18 +0800

    lite: remove mail referral ticket and order settings from admin config

--- app/Http/Controllers/V2/Admin/ConfigController.php
+++ app/Http/Controllers/V2/Admin/ConfigController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\V2\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ConfigSave;
 use App\Models\SubscribeTemplate;
-use App\Services\MailService;
 use App\Services\TelegramService;
 use App\Services\ThemeService;
 use App\Utils\Dict;
@@ -14,15 +13,6 @@ use Illuminate\Http\Request;
 class ConfigController extends Controller
 {
 
-
-    public function getEmailTemplate()
-    {
-        $path = resource_path('views/mail/');
-        $files = array_map(function ($item) use ($path) {
-            return str_replace($path, '', $item);
-        }, glob($path . '*'));
-        return $this->success($files);
-    }
 
     public function getThemeTemplate()
     {
@@ -33,22 +23,6 @@ class ConfigController extends Controller
         return $this->success($files);
     }
 
-    public function testSendMail(Request $request)
-    {
-        $mailLog = MailService::sendEmail([
-            'email' => $request->user()->email,
-            'subject' => 'This is xboard test email',
-            'template_name' => 'notify',
-            'template_value' => [
-                'name' => admin_setting('app_name', 'XBoard'),
-                'content' => 'This is xboard test email',
-                'url' => admin_setting('app_url')
-            ]
-        ]);
-        return response([
-            'data' => $mailLog,
-        ]);
-    }
     public function setTelegramWebhook(Request $request)
     {
         $hookUrl = $this->resolveTelegramWebhookUrl();
@@ -88,21 +62,6 @@ class ConfigController extends Controller
     private function getConfigMappings(): array
     {
         return [
-            'invite' => [
-                'invite_force' => (bool) admin_setting('invite_force', 0),
-                'invite_commission' => admin_setting('invite_commission', 10),
-                'invite_gen_limit' => admin_setting('invite_gen_limit', 5),
-                'invite_never_expire' => (bool) admin_setting('invite_never_expire', 0),
-                'commission_first_time_enable' => (bool) admin_setting('commission_first_time_enable', 1),
-                'commission_auto_check_enable' => (bool) admin_setting('commission_auto_check_enable', 1),
-                'commission_withdraw_limit' => admin_setting('commission_withdraw_limit', 100),
-                'commission_withdraw_method' => admin_setting('commission_withdraw_method', Dict::WITHDRAW_METHOD_WHITELIST_DEFAULT),
-                'withdraw_close_enable' => (bool) admin_setting('withdraw_close_enable', 0),
-                'commission_distribution_enable' => (bool) admin_setting('commission_distribution_enable', 0),
-                'commission_distribution_l1' => admin_setting('commission_distribution_l1'),
-                'commission_distribution_l2' => admin_setting('commission_distribution_l2'),
-                'commission_distribution_l3' => admin_setting('commission_distribution_l3')
-            ],
             'site' => [
                 'logo' => admin_setting('logo'),
                 'force_https' => (int) admin_setting('force_https', 0),
@@ -116,15 +75,10 @@ class ConfigController extends Controller
                 'tos_url' => admin_setting('tos_url'),
                 'currency' => admin_setting('currency', 'CNY'),
                 'currency_symbol' => admin_setting('currency_symbol', '¥'),
-                'ticket_must_wait_reply' => (bool) admin_setting('ticket_must_wait_reply', 0),
             ],
             'subscribe' => [
                 'plan_change_enable' => (bool) admin_setting('plan_change_enable', 1),
                 'reset_traffic_method' => (int) admin_setting('reset_traffic_method', 0),
-                'surplus_enable' => (bool) admin_setting('surplus_enable', 1),
-                'new_order_event_id' => (int) admin_setting('new_order_event_id', 0),
-                'renew_order_event_id' => (int) admin_setting('renew_order_event_id', 0),
-                'change_order_event_id' => (int) admin_setting('change_order_event_id', 0),
                 'show_info_to_server_enable' => (bool) admin_setting('show_info_to_server_enable', 0),
                 'show_protocol_to_server_enable' => (bool) admin_setting('show_protocol_to_server_enable', 0),
                 'default_remind_expire' => (bool) admin_setting('default_remind_expire', 1),
@@ -146,15 +100,6 @@ class ConfigController extends Controller
                 'server_ws_enable' => (bool) admin_setting('server_ws_enable', 1),
                 'server_ws_url' => admin_setting('server_ws_url', ''),
             ],
-            'email' => [
-                'email_host' => admin_setting('email_host'),
-                'email_port' => admin_setting('email_port'),
-                'email_username' => admin_setting('email_username'),
-                'email_password' => admin_setting('email_password'),
-                'email_encryption' => admin_setting('email_encryption'),
-                'email_from_address' => admin_setting('email_from_address'),
-                'remind_mail_enable' => (bool) admin_setting('remind_mail_enable', false),
-            ],
             'telegram' => [
                 'telegram_bot_enable' => (bool) admin_setting('telegram_bot_enable', 0),
                 'telegram_bot_token' => admin_setting('telegram_bot_token'),
@@ -170,7 +115,6 @@ class ConfigController extends Controller
                 'android_download_url' => admin_setting('android_download_url', '')
             ],
             'safe' => [
-                'email_verify' => (bool) admin_setting('email_verify', 0),
                 'safe_mode_enable' => (bool) admin_setting('safe_mode_enable', 0),
                 'secure_path' => admin_setting('secure_path', admin_setting('frontend_admin_path', hash('crc32b', config('app.key')))),
                 'email_whitelist_enable' => (bool) admin_setting('email_whitelist_enable', 0),
