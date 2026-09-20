00bcf26d4931c6690586de040c5d4ebf7e7df7c9
hahadh <hahadhlu@gmail.com>
2026-09-20 15:50:10 +0800

    lite: require invite code for every registration

--- app/Http/Requests/Passport/AuthRegister.php
+++ app/Http/Requests/Passport/AuthRegister.php
@@ -6,16 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AuthRegister extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
             'email' => 'required|email:strict',
-            'password' => 'required|min:8'
+            'password' => 'required|min:8',
+            'invite_code' => 'required|string|min:6|max:32',
         ];
     }
 
@@ -25,7 +21,8 @@ class AuthRegister extends FormRequest
             'email.required' => __('Email can not be empty'),
             'email.email' => __('Email format is incorrect'),
             'password.required' => __('Password can not be empty'),
-            'password.min' => __('Password must be greater than 8 digits')
+            'password.min' => __('Password must be greater than 8 digits'),
+            'invite_code.required' => __('You must use the invitation code to register'),
         ];
     }
 }
