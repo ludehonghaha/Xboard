7929423d3219923e4cb0f24d1118187c23ad6fa5
hahadh <hahadhlu@gmail.com>
2026-09-20 19:27:10 +0800

    lite: add NoBrand user binding model

---
+++ app/Models/NoBrandUserBinding.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class NoBrandUserBinding extends Model
+{
+    protected $table = 'v2_nobrand_user_binding';
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'display_port' => 'integer',
+        'enabled' => 'boolean',
+        'runtime_meta' => 'array',
+        'last_synced_at' => 'integer',
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+    ];
+
+    public function server(): BelongsTo
+    {
+        return $this->belongsTo(Server::class, 'server_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
