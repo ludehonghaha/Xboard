9c3b68637d7a032e5160ec16ef4ce3da1bef1e0c
hahadh <hahadhlu@gmail.com>
2026-09-20 19:28:34 +0800

    lite: route NoBrand binding reports

--- app/Http/Routes/V2/ServerRoute.php
+++ app/Http/Routes/V2/ServerRoute.php
@@ -31,6 +31,7 @@ class ServerRoute
         ], function ($route) {
             $route->post('nodes', [MachineController::class, 'nodes']);
             $route->post('nobrand-nodes', [MachineController::class, 'nobrandNodes']);
+            $route->post('nobrand-bindings', [MachineController::class, 'nobrandBindings']);
             $route->post('status', [MachineController::class, 'status']);
         });
     }
