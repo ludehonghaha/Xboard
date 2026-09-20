7e27aa9e9bacf95181f6bc821c4c17d7fce9f5f2
hahadh <hahadhlu@gmail.com>
2026-09-20 20:34:23 +0800

    lite: route NoBrand TUIC v5 creator

--- app/Http/Routes/V2/AdminRoute.php
+++ app/Http/Routes/V2/AdminRoute.php
@@ -70,6 +70,7 @@ class AdminRoute
                 $router->post('/save', [ManageController::class, 'save']);
                 $router->post('/createNoBrandSnell', [ManageController::class, 'createNoBrandSnell']);
                 $router->post('/createNoBrandHy2', [ManageController::class, 'createNoBrandHy2']);
+                $router->post('/createNoBrandTuic', [ManageController::class, 'createNoBrandTuic']);
                 $router->post('/drop', [ManageController::class, 'drop']);
                 $router->post('/copy', [ManageController::class, 'copy']);
                 $router->post('/sort', [ManageController::class, 'sort']);
